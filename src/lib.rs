721f1730e7a963f3df355292df2791c8ef15f197
Sciguymjm <sciguymjm@gmail.com>
2019-05-26 23:22:55 -0400

    Pass primitive data to Python

--- src/lib.rs
+++ src/lib.rs
@@ -1,54 +1,28 @@
 extern crate boxcars;
 extern crate failure;
+extern crate libc;
 
-use std::fs::File;
-use std::io::{Read};
-use std::os::raw::c_char;
 use std::ffi::CStr;
+use std::fs::File;
+use std::io::Read;
+use std::os::raw::c_char;
+use std::ptr::null_mut;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
 
 #[repr(C)]
 pub struct ReplayObj {
     pub header_size: i32,
     pub header_crc: i32,
     pub major_version: i32,
-    pub minor_version: i32
+    pub minor_version: i32,
 }
 
-
 #[no_mangle]
-pub extern fn print_text(text: *const c_char) {
+pub extern fn parse(text: *const c_char) -> *mut ReplayObj {
     assert!(!text.is_null());
     let c_str = unsafe { CStr::from_ptr(text) };
-    let string = c_str.to_str().expect("Not a valid UTF-8 string");
-    println!("{}", string);
-}
-
-#[no_mangle]
-pub extern fn parse(path: *const c_char) -> *mut ReplayObj {
-    let null = &mut ReplayObj {
-            header_size: 0,
-            header_crc: 0,
-            major_version: 0,
-            minor_version: 0
-    };
-    println!("Hello World!");
-    assert!(!path.is_null());
-    println!("Not null!");
-    let c_str = unsafe { CStr::from_ptr(path) };
-    println!("null!");
-    println!("{:?}", c_str.to_bytes());
-    let string = c_str.to_str().expect("Invalid UTF-8 string");
-    println!("{}", string);
     if let Ok(string) = c_str.to_str() {
-        print!("All good");
+        println!("Parsing {}", string);
         let filename = string;
         let mut f = File::open(filename).unwrap();
         let mut buffer = vec![];
@@ -61,14 +35,20 @@ pub extern fn parse(path: *const c_char) -> *mut ReplayObj {
             header_size: replay.header_size,
             header_crc: replay.header_crc,
             major_version: replay.major_version,
-            minor_version: replay.minor_version
+            minor_version: replay.minor_version,
         });
-        Box::into_raw(rrr);
+        println!("Rust Major Version {}", replay.major_version);
+//        for property in replay.properties {
+//            println!("{}", property.0);
+//            if let HeaderProp::Str(v) = property.1 {
+//                println!("{}", v);
+//            }
+//            else if let HeaderProp::Int(v) = property.1 {
+//                println!("{}", v);
+//            }
+//        }
+        Box::into_raw(rrr)
+    } else {
+        return null_mut();
     }
-    return null
-}
-
-#[no_mangle]
-pub extern "C" fn dub(x: i32) -> i32 {
-    x * 2
 }
---
+++ test.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from cffi import FFI
+
+ffi = FFI()
+# ffi.cdef("""
+#     void print_text(const char*);
+# typedef struct {
+#         int header_size, header_crc, major_version, minor_version;
+#  } ReplayObj;
+#
+# ReplayObj parse(const char*);
+# """)
+with open(r"target\debug\build\rustbucket-71f604b9f2ce340b\out\my_lib_ffi.h", "r") as f:
+    text = f.readlines()
+    text = "\n".join([l for l in text if not l.startswith("#include")])
+    ffi.cdef(text)
+    # example:
+    """
+    
+typedef struct {
+  int32_t header_size;
+  int32_t header_crc;
+  int32_t major_version;
+  int32_t minor_version;
+} ReplayObj;
+
+int32_t dub(int32_t x);
+
+ReplayObj *parse(const char *text);
+
+void print_text(const char *text);
+
+    """
+C = ffi.dlopen(r"target\debug\rustbucket.dll")
+
+
+def to_cstring(text):
+    return ffi.new("char[]", text.encode("utf-8"))
+
+replay = C.parse(to_cstring("C:\\Users\\Matt\\PycharmProjects\\boxcars\\assets\\edbb.replay"))
+
+print("Python Major Version {}".format(replay.major_version))
